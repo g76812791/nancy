d1eda2d9e11a6c23ab3599e3ecd77a8844318300
529032234@qq.com <qq18741689356>
2016-11-29 11:26:37 +0800

    test

---
+++ WebApplication19/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Nancy;
+
+namespace WebApplication19.Modules
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/{name}"] = _ =>
+            {
+                return View["/Home",_.name];
+            };
+        }
+    }
+}
